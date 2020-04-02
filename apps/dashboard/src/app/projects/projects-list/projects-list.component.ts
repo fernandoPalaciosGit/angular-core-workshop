c6eb5258131d92cd9d988eb3c4da9778c44114b4
Fernando Palacios Landi <f.palacios@avoristravel.com>
2020-04-02 17:35:52 +0200

    create subcomponents for the project module (try to solve share mutable state problem)

---
+++ apps/dashboard/src/app/projects/projects-list/projects-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'workshop-projects-list',
+  templateUrl: './projects-list.component.html',
+  styleUrls: ['./projects-list.component.css']
+})
+export class ProjectsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
