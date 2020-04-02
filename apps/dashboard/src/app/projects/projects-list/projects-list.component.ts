18ef18b2d5b64697324e53cd03cbc3b5b44cce47
Fernando Palacios Landi <f.palacios@avoristravel.com>
2020-04-02 18:15:13 +0200

    pass @Input to projects-list.component

--- apps/dashboard/src/app/projects/projects-list/projects-list.component.ts
+++ apps/dashboard/src/app/projects/projects-list/projects-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Project } from '@workshop/core-data';
 
 @Component({
   selector: 'workshop-projects-list',
@@ -6,10 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects-list.component.css']
 })
 export class ProjectsListComponent implements OnInit {
-
+  @Input() projects: Project[]; // no va a ser un observable como el del componente padre, porque lo vamos a utilizar como campo de entrada (lectura, NO va a mutar)
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
