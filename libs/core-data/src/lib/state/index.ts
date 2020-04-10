8ba1889ac399731bfa0aee92252ee565167cf9ff
Fernando Palacios Landi <f.palacios@avoristravel.com>
2020-04-10 13:43:25 +0200

    add reducer features to the App state

--- libs/core-data/src/lib/state/index.ts
+++ libs/core-data/src/lib/state/index.ts
@@ -1,13 +1,18 @@
 import { ActionReducerMap, createFeatureSelector, createSelector } from '@ngrx/store';
 
 import * as fromCustomers from './customers/customers.reducer';
+import * as fromProjects from './projects/projects.reducer';
 
+// import the type of reducers
 export interface AppState {
-  customers: fromCustomers.CustomersState
+  customers: fromCustomers.CustomersState,
+  projects: fromProjects.ProjectState,
 }
 
+// Add the reducers to the App state
 export const reducers: ActionReducerMap<AppState> = {
-  customers: fromCustomers.customersReducer
+  customers: fromCustomers.customersReducer,
+  projects: fromProjects.projectReducer
 };
 
 // -------------------------------------------------------------------
